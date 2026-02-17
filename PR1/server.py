972451131fcb86c0edc568412257f214d92bfb0b
difuz1x <klimenko1egor@gmail.com>
2026-02-18 01:03:56 +0200

    Сервер готовий

--- PR1/server.py
+++ PR1/server.py
@@ -3,22 +3,33 @@ import os
 import threading
 import server_logic
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-HOST = '127.0.0.1'
-PORT = '7777'
-s.listen(5)
-print  ("Server is listening on port {PORT}")
+def start_server(HOST = '127.0.0.1', PORT = int('7777'), max_client_amount = 1):
 
-conn, addr = s.accept()
+ s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-print ("Connected by {addr}")
+ s.bind ((HOST, PORT))
+ if (max_client_amount <1 or max_client_amount > 10):
 
-while True :
-    data = conn.recv(4096)
-    if not data: break
-    else :
-        filename = data.decode()
-        print(f"receied filename {filename} from client {addr}")
-        if file_path := server_logic.find_filename(filename):
-            
+    print("max_client_amount should be between 1 and 10")
+    return 
+ 
+ else:
+  
+  while True:
+
+    s.listen(max_client_amount)
+    print  ("Server is listening on port {PORT}")
+
+    user, addr = s.accept()
+    print ("Connected by {addr}")
+    user.send('Connection succesful')
+
+    try:
+        server_logic.send_file(user,addr)
+                
+    except Exception as err:
+     print('Error with client {addr} : {err}')  
+
+    finally : 
+     user.close()
--- PR1/server_logic.py
+++ PR1/server_logic.py
@@ -1,19 +1,44 @@
 import os
+import socket
 
-def find_filename(filename):
-    safe_filename = os.path.basename(filename)
+
+def find_filename(file_name):
+    if not file_name.strip():
+        return None
+    safe_filename = os.path.basename(file_name)
     for root, dirs, files in os.walk('.'):
         if safe_filename in files:
             full_path = os.path.join(root,safe_filename)
             if os.path.isfile(full_path):
                 print(f"File {safe_filename} was found at {full_path}")
                 return full_path
-            else :
-                print(f"File {safe_filename} was found at {full_path} but it is not a regular file.")
-                return None
-        else:
-            print(f"File {safe_filename} was not found in the current directory or its subdirectories")
-            return None
+    print(f"File {safe_filename} was not found in the current directory or its subdirectories")
+    return None
         
 
-    
+def send_file (user: socket.socket, addr: tuple,  CHUNK_SIZE: int=4096):
+   while True :
+        data = user.recv(1024)
+        if not data: break
+        else :
+
+            file_name = data.decode().strip()
+            file_path = find_filename(file_name)
+
+            if file_path:
+
+                file_size = os.path.getsize(file_path)
+                print(f"received filename {file_name} from client {addr}, file size is {file_size}")
+
+                hello_header = f"OK|{file_name}|{file_size}|{CHUNK_SIZE}|".encode('utf-8')
+
+                user.sendall(hello_header)
+
+                with open(file_path, 'rb') as file:
+                   while chunk:= file.read(CHUNK_SIZE):
+                      user.sendall(chunk)
+                print(f'Sending file to {addr} is completed')
+            else : 
+                err_header =f"ERR!|{file_name}|0|0|".encode('utf-8')
+                user.sendall(err_header)
+                
